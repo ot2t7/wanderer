b62d78156c288e61a4ed1c27eed668b5da10474c
ot2t7 <ot2t7@riseup.net>
2022-07-22 13:53:03 -0500

    bytecode processing func

--- lua/reader.lua
+++ lua/reader.lua
@@ -6,8 +6,20 @@ local bytecodeCounter = 1;
 -- If pop is true, then only the content will be popped off the 
 -- bytecode. Everything skipped leading up to the start will 
 -- be kept.
-local function popBytecode(amount, pop)
-    
+local function popBytecode(amount, pop, offset)
+    offset = offset or 0;
+    local req = string.sub(DATA_PACKED, bytecodeCounter + offset, bytecodeCounter + offset + amount - 1);
+    if pop then
+        if offset == 0 then
+            bytecodeCounter = bytecodeCounter + amount;
+            return req;
+        else 
+            DATA_PACKED = string.sub(DATA_PACKED, bytecodeCounter, bytecodeCounter + offset - 1)..string.sub(DATA_PACKED, bytecodeCounter + offset + amount, #DATA_PACKED);
+            return req;
+        end
+    else
+        return req;
+    end
 end
 
 local function readUntil(token, amount)
